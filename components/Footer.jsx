6208a578b22560718020d9352e8ca6a5d7465eee
Julian Orozco <juliancaor10@gmail.com>
2022-12-26 16:12:42 -0500

    Add Navbar and Footer Components

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,69 @@
-'use client';
+'use client'
+
+import { motion } from 'framer-motion'
+import { socials } from '../constants'
+
+import styles from '../styles'
+import { footerVariants } from '../utils/motion'
+import Image from 'next/image'
 
 const Footer = () => (
-  <footer>
-    footer
-  </footer>
-);
+  <motion.footer
+    variants={footerVariants}
+    initial='hidden'
+    whileInView='show'
+    className={`${styles.xPaddings} py-8 relative`}
+  >
+    <div className='footer-gradient' />
+    <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
+      <div className='flex items-center justify-between flex-wrap gap-5'>
+        <h4 className='font-bold md:text-[64px] text-[44px] text-white'>
+          Enter the Metaverse
+        </h4>
+        <motion.button
+          type='button'
+          className='flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]'
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 1 }}
+        >
+          <Image
+            src='/headset.svg'
+            alt='headset'
+            width={24}
+            height={24}
+            className='object-contain'
+          />
+          <span className='font-normal text-[16px] text-white'>
+            Enter Metaverse
+          </span>
+        </motion.button>
+      </div>
 
-export default Footer;
+      <div className='flex flex-col'>
+        <div className='mb-[50px] h-[2px] bg-white opacity-10' />
+
+        <div className='flex items-center justify-between flex-wrap gap-4'>
+          <h4 className='font-extrabold text-[24px] text-white'>METAVERSUS</h4>
+          <p className='font-normal text-[14px] text-white opacity-50'>
+            Copyright © 2022 Metaversus.
+          </p>
+
+          <div className='flex gap-4'>
+            {socials.map((social) => (
+              <Image
+                key={social.name}
+                src={social.url}
+                alt={social.name}
+                width={24}
+                height={24}
+                className='object-contain cursor-pointer'
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  </motion.footer>
+)
+
+export default Footer
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,40 @@
-'use client';
+'use client'
+
+import { motion } from 'framer-motion'
+
+import styles from '../styles'
+import { navVariants } from '../utils/motion'
+import Image from 'next/image'
 
 const Navbar = () => (
-  <nav>
-    navbar
-  </nav>
-);
+  <motion.nav
+    variants={navVariants}
+    initial='hidden'
+    whileInView='show'
+    viewport={{ once: true, amount: 0.25 }}
+    className={`${styles.xPaddings} py-8 relative`}
+  >
+    <div className='absolute w-[50%] inset-0 gradient-01' />
+    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
+      <Image
+        src='/search.svg'
+        alt='search'
+        width={24}
+        height={24}
+        className='object-contain'
+      />
+      <h2 className='font-extrabold text-[24px] leading-[30.24px] text-white'>
+        METAVERSUS
+      </h2>
+      <Image
+        src='/menu.svg'
+        alt='menu'
+        width={24}
+        height={24}
+        className='object-contain'
+      />
+    </div>
+  </motion.nav>
+)
 
-export default Navbar;
+export default Navbar
