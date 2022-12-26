11f359243328b8754258c722d9e0bc3b1e3944ed
Julian Orozco <juliancaor10@gmail.com>
2022-12-26 16:07:43 -0500

    Format

--- app/head.js
+++ app/head.js
@@ -1,9 +1,13 @@
 const Head = () => (
   <>
-    <title>Metaversus</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" href="/favicon.ico" />
+    <title>METAVERSUS</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1' />
+    <meta
+      mame='description'
+      content='This is the landing page for Metaversus'
+    />
+    <link rel='icon' href='/favicon.ico' />
   </>
-);
+)
 
-export default Head;
+export default Head
